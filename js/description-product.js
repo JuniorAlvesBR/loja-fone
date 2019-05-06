1b42a85ea1924800910b5dcc2b4f0f67cf6a5dc5
junioralves <jrnalves@gmail.com>
2019-05-06 20:01:28 +0000

    Aula-12 Inicio JavaScript - pegando o coração

---
+++ js/description-product.js
@@ -0,0 +1,15 @@
+/*
+1 - Ter a janela (brownser) na mão = OK - window
+2 - Ter o HTML na mão = OK - document
+3 - Pegar o Coração = OK - queryselector("classe")
+4 - Pegar o clique no coração
+5 - No momento que o usuario clicar, nós queremos mostrar um Texto na tela
+*/
+
+const $heart = window.document.querySelector(".-heart");
+
+$heart.addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log("AE");
+}
